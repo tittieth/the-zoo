16b6289c768da231288dd9f21c5e5d3c2cd5cce4
tittieth <114925706+tittieth@users.noreply.github.com>
2023-08-17 14:08:42 +0200

    get animals from LS

--- src/components/Animals.tsx
+++ src/components/Animals.tsx
@@ -7,24 +7,25 @@ import Header from './Header';
 
 function Animals() {
   const [animals, setAnimals] = useState<IAnimal[]>([]);
-  const [isDataFetched, setIsDataFetched] = useState(false);
 
   const saveToLocalStorage = (animals: IAnimal[]) => {
     localStorage.setItem('animals', JSON.stringify(animals))
   }
 
   useEffect(() => {
-    const getDataFromAPI = async () => {
-      if (!isDataFetched) {
-        const response = await getAnimals();
-        setAnimals(response);
-        saveToLocalStorage(response);
-        setIsDataFetched(true);
-      }
-    };
+    const animalsFromLS = localStorage.getItem('animals');
 
-    getDataFromAPI();
-  }, [isDataFetched]);  
+    if (animalsFromLS) {
+      setAnimals(JSON.parse(animalsFromLS))
+    } else {
+        const getDataFromAPI = async () => {
+          const response = await getAnimals();
+          setAnimals(response);
+          saveToLocalStorage(response);
+        };
+        getDataFromAPI();
+      }
+  }, []);  
 
   return (
     <>
