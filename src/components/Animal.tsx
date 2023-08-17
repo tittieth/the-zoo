adc5df401c770bccc1801c2e2c84cb786b9ab43d
tittieth <114925706+tittieth@users.noreply.github.com>
2023-08-17 14:52:15 +0200

    add styling and improved code

--- src/components/Animal.tsx
+++ src/components/Animal.tsx
@@ -2,7 +2,8 @@ import { Link, useParams } from "react-router-dom";
 import { IAnimal } from "../models/IAnimal";
 import { useEffect, useState } from "react";
 import { getAnimalById } from "../services/AnimalService";
-import { formatDateTime } from "../helpers";
+import { formatDateTime, handleImageError } from "../helpers";
+
 
 export default function Animal() {
   const [animal, setAnimal] = useState<IAnimal>();
@@ -22,16 +23,32 @@ export default function Animal() {
   });
 
   console.log(animal);
+
+  const handleClick = () => {
+    
+  }
   
 
   return (
     <>
-      <button><Link to="/animals"><img src="/public/icons8-left-64 (1).png" alt="arrow back" className="arrow"></img></Link></button>
-      <div>Animal: {id}</div>
-      <h1>{animal?.name}</h1>
-      <p>{animal?.longDescription}</p>
-      <p>Senast matad: {animal ? formatDateTime(animal.lastFed) : ""}</p>
-      <button>Mata mig!</button>
+      <button><Link to="/animals"><img src="/public/arrow-back.png" height={80} alt="arrow back" className="arrow"></img></Link></button>
+      <div className="animal-presentation-wrapper">
+        <div className="wrapper">
+          <div>
+            <h1>{animal?.name}</h1>
+            <p>{animal?.yearOfBirth}</p>
+            <p>Matad: {animal ? formatDateTime(animal.lastFed) : ""}</p>
+            <p>Hungrig: {animal?.isFed}</p>
+          </div>
+          <div>
+            <img onClick={handleClick} src={animal?.imageUrl} width={130} height={130} className="sml-img" onError={handleImageError}></img>
+          </div>
+        </div>
+        <div className="animal-desc">
+          <p>{animal?.longDescription}</p>
+          <button>Mata mig!</button>
+        </div>
+      </div>
       
     </>
   )
