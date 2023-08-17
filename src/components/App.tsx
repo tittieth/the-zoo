c6a241b8565b2a11cf16e5f765b575c508cbcdd9
tittieth <114925706+tittieth@users.noreply.github.com>
2023-08-17 10:23:50 +0200

    clean up code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,34 +1,12 @@
-import { useEffect, useState } from 'react';
 import '../App.css';
-import { IAnimal } from '../models/IAnimal';
-import { AnimalList } from './AnimalList';
-import { getAnimals } from '../services/AnimalService';
-import Header from './Header';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '../Router';
 
 function App() {
-  const [animals, setAnimals] = useState<IAnimal[]>([]);
-
-
-  useEffect(() => {
-    const getDataFromAPi = async () => {
-      const response = await getAnimals();
-      setAnimals(response)
-      console.log(response);
-      
-    }
-    getDataFromAPi();
-    
-  }, [])
-
+  
   return (
     <>
-      <div>
-        <Header />
-        <AnimalList animals={animals}/>
-        <RouterProvider router={router}></RouterProvider>
-      </div>
+      <RouterProvider router={router}></RouterProvider>
     </>
   );
 }
