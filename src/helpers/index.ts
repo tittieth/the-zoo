66e781d3d3c6b06fa8397dd59507e9980fec4814
tittieth <114925706+tittieth@users.noreply.github.com>
2023-08-18 10:03:29 +0200

    get info if an animal needs food or not

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,18 +1,24 @@
-import { IAnimal } from "../models/IAnimal";
+import { IAnimal } from '../models/IAnimal';
 
-export const handleImageError = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
-    event.currentTarget.src = "/244537-P3VK92-904.jpg";
-    event.currentTarget.alt = "Bild kunde inte laddas";
-    event.currentTarget.width = 100;
-  };
+export const handleImageError = (
+  event: React.SyntheticEvent<HTMLImageElement, Event>,
+) => {
+  event.currentTarget.src = '/244537-P3VK92-904.jpg';
+  event.currentTarget.alt = 'Bild kunde inte laddas';
+  event.currentTarget.width = 100;
+};
 
 export const formatDateTime = (isoDateTime: string) => {
-    const date = new Date(isoDateTime);
-    const formattedDate = date.toLocaleDateString("sv-SE");
-    const formattedTime = date.toLocaleTimeString("sv-SE");
-    return `${formattedDate} ${formattedTime}`;
-  };
+  const date = new Date(isoDateTime);
+  const formattedDate = date.toLocaleDateString('sv-SE');
+  const formattedTime = date.toLocaleTimeString('sv-SE');
+  return `${formattedDate} ${formattedTime}`;
+};
 
-  export const saveToLocalStorage = (animals: IAnimal[]) => {
-    localStorage.setItem('animals', JSON.stringify(animals))
-  }
+export const saveToLocalStorage = (animals: IAnimal[]) => {
+  localStorage.setItem('animals', JSON.stringify(animals));
+};
+
+export const calculateTimeSinceLastFed = (animals: IAnimal[]) => {
+    
+};
