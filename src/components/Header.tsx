f644fe87f3cc598b215c9312efb7bf4bdc9c6a96
tittieth <114925706+tittieth@users.noreply.github.com>
2023-08-22 11:22:42 +0200

    improve styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div className="header-div">
         <h1>The Zoo</h1>
-        <img src="/public/wepik-export-20230822080905LVXB.png" alt="sitting cat" width={150} height={130} loading="lazy"></img>
+        <img src="/public/wepik-export-20230822080905LVXB.png" alt="gray cat icon" width={150} height={130} loading="lazy"></img>
     </div>
   )
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,14 +1,20 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Home() {
+
+  useEffect(() => {
+    gsap.fromTo(".background-img", { y: "100%" }, { y: "0%", duration: 2 });
+  }, []);
+  
   return (
     <>
         <div className="firstpage-wrapper">
-          <div>
+          <div className="start-wrapper">
             <h1>The Zoo</h1>
             <Link to="/animals" className="start-btn">Start</Link>
           </div>
-          <div className="circle">
+          <div className="background-img">
           </div>
         </div>
     </>
