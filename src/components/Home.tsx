95db64707b454ca76a95d0e5f1e939bdaf37f54a
tittieth <114925706+tittieth@users.noreply.github.com>
2023-08-22 10:14:27 +0200

    add styling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,8 +3,14 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-        <div>Home</div>
-        <button><Link to="/animals">Start</Link></button>
+        <div className="firstpage-wrapper">
+          <div>
+            <h1>The Zoo</h1>
+            <Link to="/animals" className="start-btn">Start</Link>
+          </div>
+          <div className="circle">
+          </div>
+        </div>
     </>
   )
 }
