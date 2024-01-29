4a9372c94da9991a19ac7846c1d3d46d787c618b
tittieth <tittie.thomasson@hotmail.se>
2024-01-29 13:56:24 +0100

    Fix

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,23 +1,25 @@
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import gsap from 'gsap';
 
 export default function Home() {
-
   useEffect(() => {
-    gsap.fromTo(".background-img", { y: "100%" }, { y: "0%", duration: 2 });
+    gsap.fromTo('.background-img', { y: '100%' }, { y: '0%', duration: 2 });
   }, []);
-  
+
   return (
     <>
-        <div className="firstpage-wrapper">
-          <div className="start-wrapper">
-            <h1>The Zoo</h1>
-            <Link to="/animals" className="start-btn">Start</Link>
-          </div>
-          <div className="background-img-wrapper">
-            <div className="background-img"></div>
-          </div>
+      <div className="firstpage-wrapper">
+        <div className="start-wrapper">
+          <h1>The Zoo</h1>
+          <Link to="/animals" className="start-btn">
+            Start
+          </Link>
         </div>
+        <div className="background-img-wrapper">
+          <div className="background-img"></div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
