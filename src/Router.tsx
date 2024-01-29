8d621ebaeb76b7be8fdf1eb7671f48c65a6b5ec9
tittieth <tittie.thomasson@hotmail.se>
2024-01-29 14:10:07 +0100

    Fix: change to hashrouter

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,25 +1,25 @@
-import { createBrowserRouter } from "react-router-dom";
-import Home from "./components/Home";
-import Animals from "./components/Animals";
-import Animal from "./components/Animal";
-import NotFound from "./components/NotFound";
+import { createHashRouter } from 'react-router-dom';
+import Home from './components/Home';
+import Animals from './components/Animals';
+import Animal from './components/Animal';
+import NotFound from './components/NotFound';
 
-export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home></Home>,
-        index: true
-    },
-    {
-        path: "/animals",
-        element: <Animals></Animals>
-    }, 
-    {
-        path: "/animal/:id",
-        element: <Animal></Animal>
-    },
-    {
-        path: "*",
-        element: <NotFound />
-    }
+export const router = createHashRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    index: true,
+  },
+  {
+    path: '/animals',
+    element: <Animals></Animals>,
+  },
+  {
+    path: '/animal/:id',
+    element: <Animal></Animal>,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
 ]);
